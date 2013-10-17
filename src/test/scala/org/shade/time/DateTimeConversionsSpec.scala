fd708da42b364a05e3454213199df86fca205a62
James Shade <github@jjshade.net>
2013-10-17 07:43:38 +0100

    Completed test for DateTimeConversions

--- src/test/scala/org/shade/time/DateTimeConversionsSpec.scala
+++ src/test/scala/org/shade/time/DateTimeConversionsSpec.scala
@@ -15,93 +15,93 @@ class DateTimeConversionsSpec extends WordSpec with Matchers with MockitoSugar w
   private val gregorianGuatemala = GregorianChronology.getInstance(DateTimeZone.forID("America/Guatemala"))
   private val buddhistBudapest = BuddhistChronology.getInstance(DateTimeZone.forID("Europe/Budapest"))
 
-//  "Assigning a Time to a Long" should {
-//
-//    "Produce the right value #1" in {
-//      val value1: Long = Time(100000L)
-//      value1 shouldBe 100000L
-//    }
-//
-//    "Produce the right value #2" in {
-//      val value2: Long = Time(1381386682010L)
-//      value2 shouldBe 1381386682010L
-//    }
-//
-//    "Produce the right value #3" in {
-//      val value3: Long = Time(-1381386682010L)
-//      value3 shouldBe -1381386682010L
-//    }
-//  }
-//
-//  "Assigning a Long to a Time" should {
-//
-//    "Produce the right Time #1" in {
-//      val time: Time = 100000L
-//      time shouldBe Time(100000L)
-//    }
-//
-//    "Produce the right Time #2" in {
-//      val time: Time = 1381386682010L
-//      time shouldBe Time(1381386682010L)
-//    }
-//
-//    "Produce the right Time #3" in {
-//      val time: Time = -1381386682010L
-//      time shouldBe Time(-1381386682010L)
-//    }
-//  }
-//
-//  "Assigning a Time from a Joda DateTime" should {
-//
-//    "Produce the right Time (regardless of the chronology and timezone #1)" in {
-//      val time: Time = new DateTime(100000L, isoUtc)
-//      time shouldBe Time(100000L)
-//    }
-//
-//    "Produce the right Time (regardless of the chronology and timezone #2)" in {
-//      val time: Time = new DateTime(100000L, copticCaracas)
-//      time shouldBe Time(100000L)
-//    }
-//
-//    "Produce the right Time (regardless of the chronology and timezone #3)" in {
-//      val time: Time = new DateTime(100000L, islamicIstanbul)
-//      time shouldBe Time(100000L)
-//    }
-//
-//    "Produce the right Time (regardless of the chronology and timezone #4)" in {
-//      val time: Time = new DateTime(100000L, gregorianGuatemala)
-//      time shouldBe Time(100000L)
-//    }
-//
-//    "Produce the right Time (regardless of the chronology and timezone #5)" in {
-//      val time: Time = new DateTime(100000L, buddhistBudapest)
-//      time shouldBe Time(100000L)
-//    }
-//  }
-//
-//  "Assigning a Joda DateTime from a Time" should {
-//
-//    "Produce the right DateTime with ISO chronology and UTC timezone #1" in {
-//      val joda: DateTime = Time(100000L)
-//      joda shouldBe new DateTime(100000L, isoUtc)
-//    }
-//
-//    "Produce the right DateTime with ISO chronology and UTC timezone #2" in {
-//      val joda: DateTime = Time(2013, 10, 22, 11, 44, 33, 234, Zone("Europe/London"))
-//      joda should not be new DateTime(2013, 10, 22, 11, 44, 33, 234, DateTimeZone.forID("Europe/London"))
-//      joda shouldBe new DateTime(2013, 10, 22, 10, 44, 33, 234, isoUtc)
-//    }
-//
-//    "Produce the right DateTime with ISO chronology and UTC timezone #3" in {
-//      val joda: DateTime = Time(381386682010L)
-//      joda shouldBe new DateTime(381386682010L, isoUtc)
-//    }
-//
-//    "Produce the right DateTime with ISO chronology and UTC timezone #4" in {
-//      val joda: DateTime = Time(-381386682010L)
-//      joda shouldBe new DateTime(-381386682010L, isoUtc)
-//    }
-//  }
+  "Assigning a Time to a Long" should {
+
+    "Produce the right value #1" in {
+      val value1: Long = Time(100000L)
+      value1 shouldBe 100000L
+    }
+
+    "Produce the right value #2" in {
+      val value2: Long = Time(1381386682010L)
+      value2 shouldBe 1381386682010L
+    }
+
+    "Produce the right value #3" in {
+      val value3: Long = Time(-1381386682010L)
+      value3 shouldBe -1381386682010L
+    }
+  }
+
+  "Assigning a Long to a Time" should {
+
+    "Produce the right Time #1" in {
+      val time: Time = 100000L
+      time shouldBe Time(100000L)
+    }
+
+    "Produce the right Time #2" in {
+      val time: Time = 1381386682010L
+      time shouldBe Time(1381386682010L)
+    }
+
+    "Produce the right Time #3" in {
+      val time: Time = -1381386682010L
+      time shouldBe Time(-1381386682010L)
+    }
+  }
+
+  "Assigning a Time from a Joda DateTime" should {
+
+    "Produce the right Time (regardless of the chronology and timezone #1)" in {
+      val time: Time = new DateTime(100000L, isoUtc)
+      time shouldBe Time(100000L)
+    }
+
+    "Produce the right Time (regardless of the chronology and timezone #2)" in {
+      val time: Time = new DateTime(100000L, copticCaracas)
+      time shouldBe Time(100000L)
+    }
+
+    "Produce the right Time (regardless of the chronology and timezone #3)" in {
+      val time: Time = new DateTime(100000L, islamicIstanbul)
+      time shouldBe Time(100000L)
+    }
+
+    "Produce the right Time (regardless of the chronology and timezone #4)" in {
+      val time: Time = new DateTime(100000L, gregorianGuatemala)
+      time shouldBe Time(100000L)
+    }
+
+    "Produce the right Time (regardless of the chronology and timezone #5)" in {
+      val time: Time = new DateTime(100000L, buddhistBudapest)
+      time shouldBe Time(100000L)
+    }
+  }
+
+  "Assigning a Joda DateTime from a Time" should {
+
+    "Produce the right DateTime with ISO chronology and UTC timezone #1" in {
+      val joda: DateTime = Time(100000L)
+      joda shouldBe new DateTime(100000L, isoUtc)
+    }
+
+    "Produce the right DateTime with ISO chronology and UTC timezone #2" in {
+      val joda: DateTime = Time(2013, 10, 22, 11, 44, 33, 234, Zone("Europe/London"))
+      joda should not be new DateTime(2013, 10, 22, 11, 44, 33, 234, DateTimeZone.forID("Europe/London"))
+      joda shouldBe new DateTime(2013, 10, 22, 10, 44, 33, 234, isoUtc)
+    }
+
+    "Produce the right DateTime with ISO chronology and UTC timezone #3" in {
+      val joda: DateTime = Time(381386682010L)
+      joda shouldBe new DateTime(381386682010L, isoUtc)
+    }
+
+    "Produce the right DateTime with ISO chronology and UTC timezone #4" in {
+      val joda: DateTime = Time(-381386682010L)
+      joda shouldBe new DateTime(-381386682010L, isoUtc)
+    }
+  }
 
   "Assigning a Date from a Joda LocalDate" should {
 
@@ -134,8 +134,24 @@ class DateTimeConversionsSpec extends WordSpec with Matchers with MockitoSugar w
   "Assigning a Joda LocalDate from a Date" should {
 
     // TODO [JJS] COMPLETE TEST
-    "Produce the right LocalDate with ISO chronology and UTC timezone" in {
-      pending
+    "Produce the right LocalDate with ISO chronology and UTC timezone #1" in {
+      val joda: LocalDate = Date(2013, 11, 22)
+      joda shouldBe new LocalDate(2013, 11, 22, isoUtc)
+    }
+
+    "Produce the right LocalDate with ISO chronology and UTC timezone #2" in {
+      val joda: LocalDate = Date(1945, 12, 1)
+      joda shouldBe new LocalDate(1945, 12, 1, isoUtc)
+    }
+
+    "Produce the right LocalDate with ISO chronology and UTC timezone #3" in {
+      val joda: LocalDate = Date(1066, 1, 11)
+      joda shouldBe new LocalDate(1066, 1, 11, isoUtc)
+    }
+
+    "Produce the right LocalDate with ISO chronology and UTC timezone #4" in {
+      val joda: LocalDate = Date(1492, 12, 22)
+      joda shouldBe new LocalDate(1492, 12, 22, isoUtc)
     }
   }
 }
